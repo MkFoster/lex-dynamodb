b83d24cfc3c415b805ba72b6aa68c3315aaed6d3
Mark Foster <mark@mkfoster.com>
2018-10-09 12:37:34 -0400

    Bug fixes

--- log-intent/index.js
+++ log-intent/index.js
@@ -3,18 +3,26 @@ const aws = require('aws-sdk');
 const uuidv4 = require('uuid/v4');
 const dynamodb = new aws.DynamoDB();
 // Update the Table name below
-const tablename = process.env.TABLE_NAME;
-const url = 'http://checkip.amazonaws.com/';
-let response;
+const tableName = process.env.TABLE_NAME;
+//const tableName = 'LexLog';
+const primaryKey = process.env.PRIMARY_KEY;
+//const primaryKey = 'LexLogId';
 
-// Route the incoming request based on intent.
-// The JSON body of the request is provided in the event slot.
+/**
+ * This function expects Lex events and logs intents and
+ * session variables to DynamoDB.
+ * 
+ * Incoming events are routed based on intent.
+ */
 exports.handler = (event, context, callback) => {
     try {
         // By default, treat the user request as coming from the America/New_York time zone.
         process.env.TZ = 'America/New_York';
         console.log(`event.bot.name=${event.bot.name}`);
-        console.log(event);
+        
+        // Output the event in a JSON format we can easily copy and paste
+        // from the Cloudwatch logs to facilitate debugging.
+        console.log(JSON.stringify(event, null, 4));
 
         /**
          * Uncomment this if statement and populate with your Lex bot name, alias and / or version as
@@ -24,87 +32,68 @@ exports.handler = (event, context, callback) => {
             callback('Invalid Bot Name');
         }*/
 
+        // Get the intent name.  Intent names should match
+        // what you see in the AWS Lex Console
         const intentName = event.currentIntent.name;
 
-        // Dispatch to your skill's intent handlers
-        if (intentName === 'Greeting') {
-            return greeting(event, callback);
+        // Dispatch to the correct intent handler
+        if (intentName === 'Ping') {
+            return ping(event, callback);
         } else if (intentName === 'LogIntent') {
             return logIntent(event, callback);
         }
+        // Handle unknown intents
         throw new Error(`Intent with name ${intentName} not supported`);
-        dispatch(event, (response) => loggingCallback(response, callback));
     } catch (err) {
         callback(err);
     }
 };
 
-// --------------- Helpers that build all of the responses -----------------------
-
-function elicitSlot(sessionAttributes, intentName, slots, slotToElicit, message) {
-    return {
-        sessionAttributes,
-        dialogAction: {
-            type: 'ElicitSlot',
-            intentName,
-            slots,
-            slotToElicit,
-            message,
-        },
-    };
-}
-
-function confirmIntent(sessionAttributes, intentName, slots, message) {
-    return {
-        sessionAttributes,
-        dialogAction: {
-            type: 'ConfirmIntent',
-            intentName,
-            slots,
-            message,
-        },
-    };
-}
-
-function close(sessionAttributes, fulfillmentState, message) {
-    return {
-        sessionAttributes,
+/**
+ * This provides a quick way to check connectivity
+ * between Lex and this function.
+ */
+function ping(event, callback) {
+    let callbackObj = {
         dialogAction: {
             type: 'Close',
-            fulfillmentState,
-            message,
-        },
+            fulfillmentState: "Fulfilled",
+            message: {
+                contentType: "PlainText",
+                content: "Pong!"
+            }
+        }
     };
+    callback(null, callbackObj);
 }
 
-function delegate(sessionAttributes, slots) {
-    return {
-        sessionAttributes,
-        dialogAction: {
-            type: 'Delegate',
-            slots
-        },
-    };
-}
-
-// Route the incoming request based on intent.
-// The JSON body of the request is provided in the event slot.
+/**
+ * This is to hanlde the "LogIntent" intent.
+ * Any intent phrases from LogIntent that Lex matches will be
+ * logged here.
+ * 
+ * @param {} event 
+ * @param {*} callback 
+ */
 function logIntent(event, callback) {
     try {
         let docClient = new aws.DynamoDB.DocumentClient({region: process.env.AWS_REGION});
         
+        // Load the data we need to save from the input event into an object
         let item = {
             "currentIntent": event.currentIntent,
+            "inputTranscript": event.inputTranscript,
             "requestAttributes": event.requestAttributes,
             "sessionAttributes": event.sessionAttributes
         };
-        item[PRIMARY_KEY] = uuidv4();
+        item[primaryKey] = uuidv4();
         let params = {
-            TableName: tablename,
+            TableName: tableName,
             Item: item
         };
         
-        console.log(params);
+        // Note the data object we are sending to DynamoDB
+        console.log(JSON.stringify(params, null, 4));
 
         console.log('Putting to DynamoDB');
         docClient.put(params, function(err, data) {
@@ -119,19 +108,18 @@ function logIntent(event, callback) {
                         fulfillmentState: "Fulfilled",
                         message: {
                             contentType: "PlainText",
-                            content: "Thanks!"
+                            content: "Logged!"
                         }
                     }
                 };
-                console.log('got here!');
-                console.log(callbackObj);
+                // Save the response we are sending back to Lex in
+                // the Cloudwatch logs.
+                console.log('Logged!');
+                console.log(JSON.stringify(callbackObj, null, 4));
                 callback(null, callbackObj);
             }
         });
     } catch (err) {
         callback(err);
     }
-};
-
-
-
+}
